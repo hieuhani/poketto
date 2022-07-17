7c7d73881767568725a4d202ac8e29fcc53173c1
Hieu Tran <hieutran.fu@gmail.com>
2022-07-17 13:58:47 +0700

    initial project

---
+++ apps/chrome/src/App.tsx
@@ -0,0 +1,9 @@
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+
+export const App = () => (
+  <>
+    <CssBaseline />
+    <Button variant="contained">Hello world</Button>
+  </>
+);
